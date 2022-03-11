f0e8be597a13538cda64956e8694adbe78bc71a6
javlarpar <javier.laraparrilla@gmail.com>
2022-03-11 19:02:01 +0100

    Cambios

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8081;
 
-app.get("/", (req,res)=>{
-    console.log("Request / route");
-    res.send("<html><body><h1>"+cool()+"<h1><body><html>");
+app.use("/",express.static('public'));
+
+app.get("/faces", (req,res)=>{
+    console.log("Requested /faces route");
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
 });
+
+app.get("/time", (req,res)=>{
+    console.log("Requested /time route");
+    res.send("<html><body><h1>"+new Date()+"</h1></body></html>");
+});
+
 app.listen(port, ()=> {
-    console.log('Server ready at port ${port}');
+    console.log(`Server ready at port ${port}`);
 });
