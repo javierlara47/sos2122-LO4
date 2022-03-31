3e77a046100cab461d1f548ac379382949f7236a
javlarpar <javier.laraparrilla@gmail.com>
2022-03-31 17:26:05 +0200

    updated

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8081;
 
 app.use("/",express.static('public'));
 
-app.get("/caritas", (req,res)=>{
+app.get("/api", (req,res)=>{
     console.log("Requested / route");
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
-});
-
-app.get("/time", (req,res)=>{
-    console.log("Requested /time route");
-    res.send("<html><body><h1>"+new Date()+"</h1></body></html>");
+    res.send({result:true});
 });
 
 app.listen(port, ()=> {
