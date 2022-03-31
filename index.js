f229cf0753e38be011fc275b7e820eed02c7e88b
javlarpar <javier.laraparrilla@gmail.com>
2022-03-31 17:42:55 +0200

    updated

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.use("/",express.static('public'));
 
 app.get("/api", (req,res)=>{
     console.log("Requested / route");
-    //res.send({result:true});
-    res.sendStatus(400);
+    res.send({result:true});
 });
 
 app.listen(port, ()=> {
